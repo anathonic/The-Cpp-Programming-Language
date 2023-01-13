f0fe0fae9724996badaf030719f790f28258169d
Anath <anath.tabaczka@protonmail.com>
2023-01-13 22:45:28 +0100

    IntegerTypesSize

---
+++ Basics/IntegerTypesSize.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Size of signed char: ";
+    std::cout << sizeof(signed char) << std::endl;
+    std::cout << "Size of unsigned char: ";
+    std::cout << sizeof(unsigned char) << std::endl;
+    std::cout << "Size of short: ";
+    std::cout << sizeof(short) << std::endl;   
+    std::cout << "Size of short int: ";
+    std::cout << sizeof(short int) << std::endl;   
+    std::cout << "Size of signed short: ";
+    std::cout << sizeof(signed short) << std::endl;   
+    std::cout << "Size of signed short int: ";
+    std::cout << sizeof(signed short int) << std::endl;   
+    std::cout << "Size of unsigned short: ";
+    std::cout << sizeof(unsigned short) << std::endl;   
+    std::cout << "Size of unsigned short int: ";
+    std::cout << sizeof(unsigned short int) << std::endl;   
+    std::cout << "Size of int: ";
+    std::cout << sizeof(int) << std::endl;   
+    std::cout << "Size of signed: ";
+    std::cout << sizeof(signed) << std::endl;   
+    std::cout << "Size of signed int: ";
+    std::cout << sizeof(signed int) << std::endl;   
+    std::cout << "Size of unsigned: ";
+    std::cout << sizeof(unsigned) << std::endl;   
+    std::cout << "Size of unsigned int: ";
+    std::cout << sizeof(unsigned int) << std::endl;   
+    std::cout << "Size of long: ";
+    std::cout << sizeof(long) << std::endl;   
+    std::cout << "Size of long int: ";
+    std::cout << sizeof(long int) << std::endl;  
+    std::cout << "Size of signed long: ";
+    std::cout << sizeof(signed long) << std::endl;   
+    std::cout << "Size of signed long int: ";
+    std::cout << sizeof(signed long int) << std::endl;   
+    std::cout << "Size of unsigned long: ";
+    std::cout << sizeof(unsigned long) << std::endl;   
+    std::cout << "Size of unsigned long int: ";
+    std::cout << sizeof(unsigned long int) << std::endl;   
+    std::cout << "Size of long long: ";
+    std::cout << sizeof(long long) << std::endl;   
+    std::cout << "Size of long long int: ";
+    std::cout << sizeof(long long int) << std::endl;   
+    std::cout << "Size of signed long long: ";
+    std::cout << sizeof(signed long long) << std::endl;   
+    std::cout << "Size of signed long long int: ";
+    std::cout << sizeof(signed long long int) << std::endl;   
+    std::cout << "Size of unsigned long long: ";
+    std::cout << sizeof(unsigned long long) << std::endl;   
+    std::cout << "Size of unsigned long long int: ";
+    std::cout << sizeof(unsigned long long int) << std::endl;   
+}
